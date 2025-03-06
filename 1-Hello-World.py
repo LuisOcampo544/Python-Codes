ffeb523c38e6b413404a0782e46acfeb59eda6ce
LUIS CARLOS OCAMPO RIVERA <154545127+LuisOcampo544@users.noreply.github.com>
2025-03-05 22:43:06 -0600

    Update 1-Hello-World.py

--- 1-Hello-World.py
+++ 1-Hello-World.py
@@ -1,4 +1,4 @@
-print ('Hola Mundo') 
+print ('Hola Mundo')  
 print ('Me llamo Luis Carlos Ocampo Rivera') 
 print ('Tengo 20 años')  
 print ('Yo estudio la carrera de Ingenieria en Sistemas Computacionales en el Tecnologico de Estudios Superiores de Jilotepe')
