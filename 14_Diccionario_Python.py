caa7eef6ee6047dfd4358e8828d57d183c509296
LUIS CARLOS OCAMPO RIVERA <154545127+LuisOcampo544@users.noreply.github.com>
2025-04-18 23:15:01 -0600

    Update 14_Diccionario_Python.py

--- 14_Diccionario_Python.py
+++ 14_Diccionario_Python.py
@@ -1,4 +1,4 @@
 '''Investigar: 
 ¿Qué es un diccionario en Python?
 ¿Cuál es la finalidad al trabajar con un diccionario en Pyhon?
-¿Cómo trabajar diccionarios en Python?'''
+¿Cómo trabajar diccionarios en Python?''' 
