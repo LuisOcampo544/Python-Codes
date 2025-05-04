bab72a5790da0bfaab593726dd2c7a3a57030e46
LUIS CARLOS OCAMPO RIVERA <154545127+LuisOcampo544@users.noreply.github.com>
2025-05-03 23:09:19 -0600

    Update 13_Buscar_Informacion_Cadena.py

--- 13_Buscar_Informacion_Cadena.py
+++ 13_Buscar_Informacion_Cadena.py
@@ -1,6 +1,6 @@
 print("Buscar informacion dentro de una cadena")
 
-palabra = "Hola TEC de Jilo"  
+palabra = "Hola TEC de Jilo"   
 busca = palabra.find("TEC") 
 
 print("La palabra comienza a partir del:", busca, "caracter")
