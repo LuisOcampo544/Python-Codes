0990f94696249869690f4602073f586c81074d16
LUIS CARLOS OCAMPO RIVERA <154545127+LuisOcampo544@users.noreply.github.com>
2025-06-04 22:51:26 -0600

    Update 25_Preguntando.py

--- 25_Preguntando.py
+++ 25_Preguntando.py
@@ -3,7 +3,7 @@ while True: #Al menos una vez
     n1 = (input("Teclea un número o Presione X para salir del programa: ")) 
     if (n1 == "x"):
         break #Rompe el ciclo  
-    else: 
+    else:  
         acu = acu + int(n1) #Toma n1 como entero
 
         #Esta fuera del ciclo lo siguiente
