c1f47c11a68de327620e8711467117f1efa7d551
LUIS CARLOS OCAMPO RIVERA <154545127+LuisOcampo544@users.noreply.github.com>
2025-06-06 23:45:17 -0600

    Update 29_Interfaz_Grafica.py

--- 29_Interfaz_Grafica.py
+++ 29_Interfaz_Grafica.py
@@ -3,4 +3,4 @@ ventana = Tk() #La variable ventana toma el valor del modo grafico (Ventana)
 ventana.title ("Tecnologico")
 ventana.resizable (True, True) #Se puede ampliar horizontal y verticalmente
 ventana.geometry ("650x400") #La ventana se define 650 por 400 pixeles
-ventana.mainloop () 
+ventana.mainloop ()  
